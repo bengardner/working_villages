c08b3d9fd8da648b707a78fd5d6e63fdee8f7a9c
Ben Gardner <gardner.ben@gmail.com>
2022-10-14 20:31:13 -0500

    job_coroutines.lua: whitespace/indent cleanup

--- working_villagers/job_coroutines.lua
+++ working_villagers/job_coroutines.lua
@@ -12,36 +12,36 @@ job_coroutines.commands = commands
 local log = working_villages.require("log")
 
 function job_coroutines.resume(self,dtime)
-  local job = self:get_job()
-  if not job then return end
-  if not self.job_thread then
-    if job.on_step then
-      job.on_start(self)
-      self.job_thread = coroutine.create(job.on_step)
-    elseif job.jobfunc then
-      self.job_thread = coroutine.create(job.jobfunc)
-    else
-      log.error("villager %s is running an invalid job",self.inventory_name)
-    end
-  end
-  if coroutine.status(self.job_thread) == "dead" then
-    if job.jobfunc then
-      self.job_thread = coroutine.create(job.jobfunc)
-    else
-      self.job_thread = coroutine.create(job.on_step)
-    end
-  end
-  if coroutine.status(self.job_thread) == "suspended" then
-    local ret = {coroutine.resume(self.job_thread, self, dtime)}
-    if ret[1] then
-      if ret[2] == commands.pause then
-       self:set_pause(true)
-       self:set_displayed_action(ret[3])
-      end
-    else
-      error("error in job_thread " .. ret[2]..": "..debug.traceback(self.job_thread))
-    end
-  end
+	local job = self:get_job()
+	if not job then return end
+	if not self.job_thread then
+		if job.on_step then
+			job.on_start(self)
+			self.job_thread = coroutine.create(job.on_step)
+		elseif job.jobfunc then
+			self.job_thread = coroutine.create(job.jobfunc)
+		else
+			log.error("villager %s is running an invalid job",self.inventory_name)
+		end
+	end
+	if coroutine.status(self.job_thread) == "dead" then
+		if job.jobfunc then
+			self.job_thread = coroutine.create(job.jobfunc)
+		else
+			self.job_thread = coroutine.create(job.on_step)
+		end
+	end
+	if coroutine.status(self.job_thread) == "suspended" then
+		local ret = {coroutine.resume(self.job_thread, self, dtime)}
+		if ret[1] then
+			if ret[2] == commands.pause then
+				self:set_pause(true)
+				self:set_displayed_action(ret[3])
+			end
+		else
+			error("error in job_thread " .. ret[2]..": "..debug.traceback(self.job_thread))
+		end
+	end
 end
 
 return job_coroutines
